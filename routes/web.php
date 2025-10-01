820faa7f44382f9eb0f4f7c87e4e89068a12e2c1
Hiroki <baseballinobu@gmail.com>
2025-10-02 07:33:47 +0900

    initial commit

---
+++ routes/web.php
@@ -0,0 +1,37 @@
+<?php
+
+use App\Http\Controllers\ProfileController;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    // fakerオブジェクトを生成します
+    $faker = fake();
+    // 4から10のランダムな文を生成します
+    $chatMessages = $faker->sentences($faker->numberBetween(4, 10));
+    // 'welcome'ビューを表示し、生成したチャットメッセージをビューに渡します
+    return view('welcome', ['chatMessages'=>$chatMessages]);
+});
+
+Route::get('/dashboard', function () {
+    $faker = fake();
+    return view('dashboard',[
+        'welcomeMessages' => $faker->paragraphs(5),
+    ]);
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::get('/about', function () {
+    // fakerオブジェクトを生成します
+    $faker = fake();
+    // 4から10のランダムな文を生成します
+    $chatMessages = $faker->sentences($faker->numberBetween(4, 10));
+    // 'welcome'ビューを表示し、生成したチャットメッセージをビューに渡します
+    return view('welcome', ['chatMessages'=>$chatMessages]);
+});
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
